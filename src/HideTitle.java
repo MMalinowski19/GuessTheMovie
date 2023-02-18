a97138336b8671486f3e0d0a7872ea18260d1106
MMalinowski19 <mikolajmalinowski19@gmail.com>
2023-02-18 17:40:41 +0100

    Program is hiding titles

--- src/HideTitle.java
+++ src/HideTitle.java
@@ -1,66 +1,50 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class HideTitle {
 
     String title;
     String str;
-    String str1;
-    String str2;
-    String str3;
-    String letter;
-    //String[] tab;
+    char letter;
+    void hideTit() {
+        str = title.replaceAll("[a-zA-Z]", "_");
+        System.out.println(str);
+    }
 
-void hideTit(){
-    str = title.replaceAll("[a-zA-Z]","_");
-    System.out.println(str);
-
-}
-void guessTheMovie(){
-
-
-    while (str.contains("_")) {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the letter:");
-        letter = scan.nextLine();
-        char [] tab = new char[title.length()];
-        char [] tabstr = new char[str.length()];
-        for (int i=0; i<title.length(); i++) {
+    void guessTheMovie() {
+        char[] tab = new char[title.length()];
+        char[] tabstr = new char[str.length()];
+        for (int i = 0; i < title.length(); i++) {
             tab[i] = title.charAt(i);
         }
-        for (int i=0; i<str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             tabstr[i] = str.charAt(i);
         }
-        if (title.contains(letter)) {
-            char[] charls = letter.toCharArray();
-            //str1 = title.replaceAll(letter,"1");
-            //str2 = str1.replaceAll("[a-zA-Z]","_");
-            //str3 = str2.replaceAll("1",letter);
-            //System.out.println(str1);
-            //System.out.println(str2);
-            //char [] tab = new char[title.length()];
-            //for (int i=0; i<title.length(); i++){
-              //  tab[i]=title.charAt(i);
-            for (int i=0; i<title.length(); i++){
-                if (charls[1]!=tab[i]){
-                    tab[i] = tabstr[i];
+        while (str.contains("_")) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the letter:");
+            //letter = scan.nextLine();
+            letter = scan.next().charAt(0);
+
+            for (int i = 0; i < title.length(); i++) {
+                if (tab[i] == letter){
+                    tabstr[i]=letter;
+                    System.out.println(tabstr);
+                    //tabstr.toString();
+                    //str = tabstr
 
                 }
 
+               // System.out.print(tab[i] + " ");
             }
-            }
-         //   tab [n] = title;
-          //  str = str3;
-            for (int i=0; i<title.length(); i++){
-
-                System.out.print(tab[i]+" ");
-            }
-        //    System.out.println(tab[3]);
-        break;
-    }
-    //System.out.println(str);
-        //System.out.println("Brawo! You guess the title!");
-
+            //    System.out.println(tab[3]);
+           // break;
+        }
+        System.out.println("Brawo! You guess the title!");
     }
 
 }
+
+
+
